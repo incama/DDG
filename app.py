cfe2b5b8e099d7e580f9b983dc9acfc1b5457edd
Incama <incama@gmail.com>
2024-12-31 09:41:37 +0100

    Init.

--- app.py
+++ app.py
@@ -336,6 +336,12 @@ def gallery_file(filepath):
     print(f"Serving file: {full_path}")
     return send_from_directory(BASE_DIR, filepath)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=5000, debug=False)
 
